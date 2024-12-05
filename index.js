be73ccb8ae2b9d03d0dd6e42ef4b13eb7fb5a776
PriyaS2200 <sonkarpriya667@gmail.com>
2024-12-05 23:06:00 +0530

    feat/findEvenodd

--- index.js
+++ index.js
@@ -17,3 +17,17 @@ console.log(findPrime(33))
 console.log(findPrime(43))
 console.log(findPrime(12))
 console.log(findPrime(15))
+
+function evenOdd(num){
+   if(num%2 == 0){
+    return true;
+   }
+   else{
+    return false;
+   }  
+}
+console.log(evenOdd(13))
+console.log(evenOdd(10))
+console.log(evenOdd(72))
+console.log(evenOdd(67))
+console.log(evenOdd(90))
